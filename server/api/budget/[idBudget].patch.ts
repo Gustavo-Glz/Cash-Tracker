14316b91a79dc3bbd0552cc9e9cc4291a3dad769
Gustavo-Glz <gustavo.gonzalez.montalvo@gmail.com>
2026-06-04 15:30:19 -0600

    update(budget): update delete and update budget endpoint

--- server/api/budget/[idBudget].patch.ts
+++ server/api/budget/[idBudget].patch.ts
@@ -1,8 +1,9 @@
 export default defineEventHandler(async (event) => {
+  const { user } = await getUserSession(event)
   const id = getRouterParam(event, 'idBudget')
   const { idBudget: validatedId } = paramsSchema.pick({ idBudget: true }).parse({ idBudget: id })
   const body = await readValidatedBody(event, budgetSchema.parse)
-  const budget = await budgetRepository.findById(validatedId)
+  const budget = await budgetRepository.findById(validatedId, user!.id)
   if (!budget) {
     throw createError({ statusCode: 404, statusMessage: 'Expense not found' })
   }
