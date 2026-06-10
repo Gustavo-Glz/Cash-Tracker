9052c9635bddd97afa0f4bf0aa2e020f5f8cb222
Gustavo-Glz <gustavo.gonzalez.montalvo@gmail.com>
2026-06-10 12:46:49 -0600

    feat: add footer page

---
+++ app/components/shared/Footer.vue
@@ -0,0 +1,6 @@
+<template>
+  <USeparator type="dotted" class="mt-10 h-px" />
+  <UFooter class="py-10">
+    <p class="text-muted text-sm">Todos los derechos reservados © {{ new Date().getFullYear() }}</p>
+  </UFooter>
+</template>
